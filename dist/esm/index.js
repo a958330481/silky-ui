ff25955c6f8b6f59171ccc9d60e0f181493f48ed
zhangkai14 <zhangkai14@corp.netease.com>
2022-09-21 11:32:02 +0800

    feat: 基于dumi搭建项目脚手架

---
+++ dist/esm/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Descripttion:
+ * @Author: zhangkai14@corp.netease.com
+ * @Date: 2022-09-21 11:15:00
+ * @LastEditors: zhangkai14@corp.netease.com
+ * @LastEditTime: 2022-09-21 11:22:43
+ */
+export { default as Foo } from './Foo';
